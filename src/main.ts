62419e32c7e2930a1ffccca1aff68bde0a0ba17b
yassineoo <jy_attou@esi.dz>
2025-09-06 13:44:50 +0100

    fix add agent

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true, // allow all origins
+    credentials: true,
+  });
   app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const config = new DocumentBuilder()
