006e57743e788a80dfb0a2eeb0f56198d02d3061
yassineoo <jy_attou@esi.dz>
2025-11-24 11:18:34 +0100

    add hardware to module

--- src/lockers/lockers.module.ts
+++ src/lockers/lockers.module.ts
@@ -5,10 +5,12 @@ import { Locker } from './entities/locker.entity';
 import { City } from 'src/wilaya/entities/city.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
+import { LockerHardwareController } from './locker-hardware.controller';
+import { LockerHardwareService } from './locker-hardware.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Locker, City]), UsersModule],
-  controllers: [LockersController],
-  providers: [LockersService],
+  controllers: [LockersController, LockerHardwareController],
+  providers: [LockersService, LockerHardwareService],
 })
 export class LockersModule {}
